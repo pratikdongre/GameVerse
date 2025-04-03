42061ae051c86da9a65d4bbdb25c2b731e0d6cd9
pratik dongre <pratikdon2001@gmail.com>
2025-04-03 17:14:50 +0530

    Shows Spinner while Fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,26 +2,33 @@ import React from "react";
 // import useGenre from "../hooks/useGenre";
 import useGenre, { Genre } from "../hooks/useGenre";
 import useData from "../hooks/useData";
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
-  const { data } = useGenre();
+  const { data, isLoading, error } = useGenre();
+
+  if (error) return null;
+  //   if (isLoading) return <Spinner></Spinner>
 
   return (
     <List.Root>
-      {data.map((genre) => (
-        <ListItem key={genre.id}>
-          <HStack marginY={2}>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            ></Image>
-            <Text fontSize="17px">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
+      {isLoading ? (
+        <Spinner color="pink.400" size="lg" />
+      ) : (
+        data.map((genre) => (
+          <ListItem key={genre.id}>
+            <HStack marginY={2}>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              ></Image>
+              <Text fontSize="17px">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))
+      )}
     </List.Root>
   );
 }
