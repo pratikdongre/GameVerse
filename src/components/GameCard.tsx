08b7bbd545540f93f074eca796378854a45eb5c0
pratik dongre <pratikdon2001@gmail.com>
2025-04-06 10:30:04 +0530

    Add Emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -6,28 +6,32 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+  // console.log("Rating Top:", game.rating_top);
+
   return (
     <Card.Root maxW="sm" overflow="hidden" borderRadius="15px">
       {/* // <Card.Root> */}
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name} </Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
 
         {/* {game.parent_platforms.map(({ platform }) => (
           <Text>{platform.name}</Text>
         ))} */}
 
-        <HStack justify={"space-between"}>
+        <HStack justify={"space-between"} marginBottom={4}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card.Root>
   );
